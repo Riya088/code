bb479309782a34d74e98e377c92f3b5734b1586c
Riya088 <riyasachan818@gmail.com>
2023-01-19 18:06:30 +0530

    age

---
+++ age.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class age {
+    public static void main(String[] args) {
+        Scanner s=new Scanner (System.in);
+        int age;
+        System.out.println("Enter age:");
+        age=s.nextInt();
+        System.out.println("Your age is"+age);
+    }
+}
